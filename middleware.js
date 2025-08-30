92a00765d6d68eee5d97740bf4b0c34cc88f7b43
vivek <vivekgirhepunje3456@gmail.com>
2025-08-30 15:10:30 +0530

    Authentication Completed

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
 module.exports.isLoggedIn = (req,res,next)=>{
+    
     if(!req.isAuthenticated()){
+        //if user is not logged then only we will save req.originalUrl to save where user was 
+        // previously requesting 
+        req.session.redirectUrl=  req.originalUrl;
         req.flash("error","You must be logged in to perform this operation!!")
-        return res.redirect('/login') 
+        return  res.redirect("/login") 
     }
     next()
 }
 
+module.exports.saveRedirectUrl = (req,res,next)=>{
+    if(req.session.redirectUrl){
+    res.locals.redirectUrl= req.session.redirectUrl;
+    }
+    next()
+}
--- routes/uesr.js
+++ routes/uesr.js
@@ -3,7 +3,7 @@ const router= express.Router()
 const User= require('../models/user')
 const { wrapAsync } = require('../utils/wrapAsync')
 const passport = require('passport')
-
+const {saveRedirectUrl}= require('../middleware')
 // for signup
 router.get('/signup',async (req,res)=>{
     res.render('./users/signup')
@@ -15,9 +15,14 @@ router.post('/signup',wrapAsync(async(req,res)=>{
     const {username,email,password}= req.body
     let newUser= new User({email,username})
    let user= await User.register(newUser,password)
-   req.flash("success","User has successfully Registered!")
-   console.log(user)
-   res.redirect('/listings')
+   req.login(user,(err)=>{
+    if(err){
+        return next(err)
+    }
+    req.flash("success","User has successfully Registered!")
+    console.log(user)
+    res.redirect('/listings')
+   })
     }
     catch(err)
     {
@@ -33,12 +38,22 @@ router.get('/login',async (req,res)=>{
     res.render('./users/login')
 })
 
-router.post('/login',passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async (req,res)=>{
+router.post('/login',saveRedirectUrl,passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async (req,res)=>{
     req.flash("success","Welcome to Wanderlust!")
-    res.redirect("/listings")
+    const redirectUrl= res.locals.redirectUrl||'/listings'
+    res.redirect(redirectUrl) 
 })
 
-
+router.get('/logout',(req,res,next)=>{
+    req.logout((err)=>{
+       if(err){
+         return next(err)
+       }
+       req.flash("success","you logged out!")
+       res.redirect("/listings")
+    })
+    
+})
 
 
 module.exports= router
